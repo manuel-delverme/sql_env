fix logprob calculation

ppo/model.py
@@ -46,7 +46,7 @@ class Policy(nn.Module):
         embeds = self.html_to_embedd(batch_response)
         value, query, query_logprobs = self.base(embeds)
         query = [self.output_vocab[int(q)] for q in query]
-        return value, np.array(query).reshape(1, 1, -1), torch.tensor(query_logprobs).reshape(1, 1, -1)
+        return value, np.array(query).reshape(1, 1, -1), torch.stack(query_logprobs)
 
     def html_to_embedd(self, batch_response):
         word_idxes = []
@@ -61,9 +61,20 @@ class Policy(nn.Module):
         value, _, _ = self.base(embeds)
         return value
 
-    def evaluate_actions(self, batch_response, action):
+    def evaluate_actions(self, batch_response, actions):
         embeds = self.html_to_embedd(batch_response)
+
+        # query = [self.output_word_to_idx[q] for q in action for action in actions]
         value, query, query_logprobs = self.base(embeds)
+        query_logprobs = torch.stack(query_logprobs, dim=1)
+        parsed_actions = []
+        for action in actions:
+            for q in action:
+                action_idx = self.output_word_to_idx[q]
+                action_vector = torch.zeros(size=(query_logprobs.shape[-1],))
+                action_vector[action_idx] = 1
+                parsed_actions.append(action_vector)
+        parsed_actions = torch.stack(parsed_actions, dim=0).reshape(query_logprobs.shape)
         # dist = self.dist(actor_features)
         # action = action.unsqueeze(0)
         # shifted_idx = list(range(action.dim()))
@@ -73,12 +84,11 @@ class Policy(nn.Module):
         # one_hot_actions = action.new(sample_shape).zero_()
         # one_hot_actions.scatter_add_(-1, action, torch.ones_like(action))
 
-        action_log_probs = torch.stack(query_logprobs, dim=1)
+        # action_log_probs = torch.stack(query_logprobs, dim=1)
         # torch.softmax(query_logprobs)
         # dist_entropy = dist.entropy().mean()
-        dist_entropy = torch.ones(1)
 
-        return value, action_log_probs, dist_entropy
+        return value, query_logprobs, parsed_actions
 
 
 class NNBase(nn.Module):
@@ -120,20 +130,18 @@ class MLPBase(NNBase):
         _x, rnn_hxs = self.gru(hist, None)  # (seq_len, batch, input_size)
         rnn_hxs = rnn_hxs.squeeze(0)
         # TODO: this was _x instead of rnn_hxs, but i want to remove the time dimension
-
+        # TODO fix this with logprop
         value = self.critic(rnn_hxs)
         query = []
         query_logprobs = []
 
         for _ in range(4):
             word_logprobs, rnn_hxs = self.actor(rnn_hxs)
-            word = torch.argmax(word_logprobs)
-            if word == 0.:
-                break
-            else:
-                query.append(word)
-                query_logprobs.append(word_logprobs[:, word])
-        print(query)
+            # word = torch.argmax(word_logprobs)
+            word = torch.distributions.Categorical(logits=word_logprobs).sample((1,)).squeeze()
+            query.append(word)
+            query_logprobs.append(word_logprobs)
+
         return value, query, query_logprobs
 
 