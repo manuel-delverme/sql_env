2c54fe2f00eb892239aae73a4158cc37176087eb
avalds <avalds@gmail.com>
2021-06-06 11:11:20 +0200

    Now generate some structured actions

--- environment/__init__.py
+++ environment/__init__.py
@@ -1,10 +1,18 @@
 import gym.envs
 
 import environment.sql_env
+import environment.sql_env2
 
 gym.envs.register(
     id='SQL-v0',
     entry_point='environment.sql_env:SQLEnv',
+    max_episode_steps=10,
+    kwargs={'html': False}
+)
+
+gym.envs.register(
+    id='SQL-v1',
+    entry_point='environment.sql_env2:SQLEnv',
     max_episode_steps=10,
     kwargs={'html': False}
 )
