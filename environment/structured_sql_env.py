240fe643a60d1ceca15aad487d95837c90ae97f4
manuel <manuel.delverme@gmail.com>
2021-06-14 00:55:38 +0200

    refactor

--- environment/structured_sql_env.py
+++ environment/structured_sql_env.py
@@ -1,56 +1,49 @@
 import http.client
 import http.server
-import re
 import sqlite3
-import subprocess
-import time
-import traceback
-import urllib.parse
-import urllib.request
 import xml.etree.ElementTree
 
 import gym.envs
-from gym import spaces
 import numpy as np
 import torch.distributions
+from gym import spaces
 
 import constants
+from structured.generate_actions import generate_actions
 
 torch.manual_seed(1)
-"""
-XXX We might need a new state as it has to remember some things from the previous queries, like the escape character (and maybe the number of rows, but it can also just go directly.)
-"""
+structured_actions = generate_actions(max_columns=constants.max_columns)
 
 
 class SQLEnvStructured(gym.Env):
     def render(self, mode='human'):
         pass
 
-    def __init__(self, max_columns = 3):
+    def __init__(self, max_columns=3):
         """
-    	Description:
-    		A webserver exposing a query with a potential SQL injection vulnerability. Behind the vulnerability lies a flag.
-    	Observation:
-    		Type: MiltiDiscrete(4)
-    		Num    Observation
-    		0   action tried and returned an error
+        Description:
+            A webserver exposing a query with a potential SQL injection vulnerability. Behind the vulnerability lies a flag.
+        Observation:
+            Type: MiltiDiscrete(4)
+            Num    Observation
+            0   action tried and returned an error
             1   action never tried
-    		2   action tried and returned an empty string
-    		3   action tried and returned something
+            2   action tried and returned an empty string
+            3   action tried and returned something
             (4  action tried and we found the flag)
-    	Actions:
-    		Type: Discrete(n)
-    		Num    Action
-    		n    SQL statement n
-    	Reward:
-    		+100 for capturing the flag, -1 in all the other cases.
-    	Starting State:
-    		Webserver initialized with a random query. No action tested.
-    	Episode Termination:
-    		Capture the flag.
-    	"""
+        Actions:
+            Type: Discrete(n)
+            Num    Action
+            n    SQL statement n
+        Reward:
+            +100 for capturing the flag, -1 in all the other cases.
+        Starting State:
+            Webserver initialized with a random query. No action tested.
+        Episode Termination:
+            Capture the flag.
+        """
         self.max_columns = max_columns
-        #We can use the same database as long as we change the hidden query
+        # We can use the same database as long as we change the hidden query
         http.server.HTTPServer.allow_reuse_address = True
         self.connection = sqlite3.connect(":memory:", isolation_level=None, check_same_thread=False)
 
@@ -61,7 +54,7 @@ class SQLEnvStructured(gym.Env):
         self.cursor.execute("INSERT INTO private(id, userid, account) VALUES(NULL, 1, 'accountnr:123456')")
 
         data = []
-        #To tell the agent what kind of outputs it can expect (XXX so far this is not an exhaustive list)
+        # To tell the agent what kind of outputs it can expect (XXX so far this is not an exhaustive list)
         output_vocab = {"UNION", "SELECT", "*", "FROM", "users", "1", "ERROR", "", "private", "account", "None", "and"}
 
         for idx, row in enumerate(xml.etree.ElementTree.fromstring(constants.USERS_XML).findall("user")):
@@ -70,33 +63,32 @@ class SQLEnvStructured(gym.Env):
             output_vocab.update(row)
             output_vocab.update(str(idx + 1))
 
-        #Not truly the action space, but mainly to tell the agent what to expect?
-        self.action_space = spaces.Discrete(len(constants.structured_actions))
+        # Not truly the action space, but mainly to tell the agent what to expect?
+        self.action_space = spaces.Discrete(len(structured_actions))
 
-        #Observation Space
-        self.observation_space = spaces.MultiDiscrete(np.ones(len(constants.structured_actions))*4)
+        # Observation Space
+        self.observation_space = spaces.MultiDiscrete(np.ones(len(constants.structured_actions)) * 4)
 
         self.cursor.executemany("INSERT INTO users(id, username, firstname, surname, age, nationality, created_at) VALUES(NULL, ?, ?, ?, ?, ?, ?)", data)
         self.cursor.execute("CREATE TABLE comments(id INTEGER PRIMARY KEY AUTOINCREMENT, comment TEXT, time TEXT)")
 
-        #We create the hidden_query in the reset function
+        # We create the hidden_query in the reset function
         self.reset()
 
-
     def step(self, action):
-        assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
-        inquery = constants.structured_actions[action]
+        assert self.action_space.contains(action), "%r (%s) invalid" % (action, type(action))
+        inquery = structured_actions[action]
         assert isinstance(inquery, str)
         code = http.client.OK
         content = ""
-        query = self.hidden_query.format(input = inquery)
+        query = self.hidden_query.format(input=inquery)
         try:
             self.cursor.execute(query)
             for some in self.cursor.fetchall():
                 for f in some:
-                    content += str(f) + ";"#"{'-' if f is None else f}\n"
+                    content += str(f) + ";"  # "{'-' if f is None else f}\n"
         except Exception as ex:
-            #print(ex)
+            # print(ex)
             content += "ERROR"
             code = http.client.INTERNAL_SERVER_ERROR
 
@@ -107,43 +99,36 @@ class SQLEnvStructured(gym.Env):
             reward = -1.
         else:
             reward = -.1
-            if(content == ""):
+            if (content == ""):
                 response = 2
             else:
                 response = 3
-        if('account' in content):
+        if ('account' in content):
             reward = 100
             response = 3
             terminal = True
 
         self.state[action] = response
-        #return content, reward, terminal, {}
+        # return content, reward, terminal, {}
         return self.state, reward, terminal, {'msg': "Server response{}".format(response)}
-
 
     def reset(self):
         columns = constants.columns
-        escape_characters = ["'", '"',""]
+        escape_characters = ["'", '"', ""]
 
-        #Picking the number of columns for the hidden query
-        self.colnum = np.random.choice(self.max_columns)+1
-        #Picking the escape_character for the hidden query
+        # Picking the number of columns for the hidden query
+        self.colnum = np.random.choice(self.max_columns) + 1
+        # Picking the escape_character for the hidden query
         self.escape = np.random.choice(3)
-        #constructing the hidden query
-        #Simulating that we are logged in as the dummy user Bob for this attack
-        if(self.escape == 0):
-            self.hidden_query = "SELECT "+", ".join(columns[:self.colnum])+" FROM users WHERE firstname={0}{1}{0}".format(escape_characters[self.escape], "{input}")
-        elif(self.escape == 1):
-            self.hidden_query = "SELECT "+", ".join(columns[:self.colnum])+" FROM users WHERE nationality={0}{1}{0}".format(escape_characters[self.escape], "{input}")
+        # constructing the hidden query
+        # Simulating that we are logged in as the dummy user Bob for this attack
+        if (self.escape == 0):
+            self.hidden_query = "SELECT " + ", ".join(columns[:self.colnum]) + " FROM users WHERE firstname={0}{1}{0}".format(escape_characters[self.escape], "{input}")
+        elif (self.escape == 1):
+            self.hidden_query = "SELECT " + ", ".join(columns[:self.colnum]) + " FROM users WHERE nationality={0}{1}{0}".format(escape_characters[self.escape], "{input}")
         else:
-            self.hidden_query = "SELECT "+", ".join(columns[:self.colnum])+" FROM users WHERE age={0}{1}{0}".format(escape_characters[self.escape], "{input}")
+            self.hidden_query = "SELECT " + ", ".join(columns[:self.colnum]) + " FROM users WHERE age={0}{1}{0}".format(escape_characters[self.escape], "{input}")
 
-        #state, _, _, _ = self.step("1")
-        self.state = np.ones(len(constants.structured_actions))
+        # state, _, _, _ = self.step("1")
+        self.state = np.ones(len(structured_actions))
         return self.state
-
-
-
-if(__name__ == "__main__"):
-    s = SQLEnv()
-    s.step("test")
