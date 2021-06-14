3cd5671cb787c7071de1bbbd3932d10487d0337d
manuel <manuel.delverme@gmail.com>
2021-06-14 04:25:13 +0200

    [CLUSTER] crashycrash

--- ppo/model.py
+++ ppo/model.py
@@ -1,5 +1,6 @@
-import numpy as np
 import string
+
+import numpy as np
 import torch
 import torch.nn as nn
 
@@ -9,12 +10,12 @@ class Policy(nn.Module):
         super(Policy, self).__init__()
         EMBEDDING_DIM = 10
 
-        base_query = set(string.ascii_lowercase + " ").union({" SELECT ", " FROM ", " * "})
+        action_vocab = set(string.ascii_lowercase + " ").union({" UNION SELECT ", " NULL ", " FROM "})
         query_vocab = sorted(output_vocab)
-        output_vocab = sorted(base_query)
+        output_vocab = sorted(action_vocab)
 
         self.query_word_to_idx = {word: idx for idx, word in enumerate(query_vocab)}
-        self.output_word_to_idx = {word: idx for idx, word in enumerate(output_vocab)}
+        self.output_token_to_idx = {word: idx for idx, word in enumerate(output_vocab)}
 
         self.embeddings_in = nn.Embedding(len(query_vocab), EMBEDDING_DIM)
         self.embeddings_out = nn.Embedding(len(output_vocab), EMBEDDING_DIM)
@@ -30,7 +31,7 @@ class Policy(nn.Module):
         return " ".join([self.query_vocab[w] for w in action])
 
     def _encode(self, state):
-        retr = torch.tensor([self.output_word_to_idx[w] for w in state], dtype=torch.long)
+        retr = torch.tensor([self.output_token_to_idx[w] for w in state], dtype=torch.long)
         return retr
 
     def act(self, batch_response):
@@ -39,22 +40,23 @@ class Policy(nn.Module):
         batch_size, query_length, _ = batch_query.shape
         queries = []
         for query_idx in batch_query:
-            query_txt = [self.output_vocab[idx] for idx in query_idx]
-            queries.append(query_txt)
+            query_tokens = [self.output_vocab[idx] for idx in query_idx]
+            queries.append(query_tokens)
         return value, np.array(queries), query_logprobs
 
     def html_to_embedd(self, batch_response):
         word_embeddings = []
         for response in batch_response:
+            assert len(response) == 1
             for content in response:
-                if len(content) > 0:
+                if not content:
+                    sentence_idxs = [self.query_word_to_idx[""]]
+                else:
                     content = content.strip().split()
                     sentence_idxs = []
                     for word in content:
                         sentence_idxs.append(self.query_word_to_idx[word])
-                else:
-                    #Added to deal with the response being an empty string (i.e. len(content) == 0)
-                    sentence_idxs = [self.query_word_to_idx[""]]
+
                 embeds = self.embeddings_in(torch.tensor(sentence_idxs))
                 word_embeddings.append(embeds)
         assert len(word_embeddings) == len(batch_response)
@@ -73,9 +75,9 @@ class Policy(nn.Module):
         # here we extend in the time domain instead as we ware batching
         value, query, query_logprobs = self.base(embeds)
         parsed_actions = []
-        for action in actions:
-            for q in action:
-                action_idx = self.output_word_to_idx[q]
+        for token_sequence in actions:
+            for token in token_sequence:
+                action_idx = self.output_token_to_idx[token]
                 action_vector = torch.zeros(size=(query_logprobs.shape[-1],))
                 action_vector[action_idx] = 1
                 parsed_actions.append(action_vector)
