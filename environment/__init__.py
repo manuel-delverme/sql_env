aa36f524c7c428e59cd4b6c54a38283998979e24
manuel <manuel.delverme@gmail.com>
2021-06-14 16:50:19 +0200

    fixed errors

--- environment/__init__.py
+++ environment/__init__.py
@@ -10,12 +10,10 @@ gym.envs.register(
     id='SQL-v1',
     entry_point='environment.sql_env:SQLEnv',
     max_episode_steps=config.rate_limit,
-    kwargs={"max_columns": constants.max_columns}
 )
 
 gym.envs.register(
     id='SQLstruct-v1',
     entry_point='environment.structured_sql_env:SQLEnvStructured',
     max_episode_steps=config.rate_limit,
-    kwargs={"max_columns": constants.max_columns}
 )
